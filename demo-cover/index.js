d3323aa4cfb5a90231fe415b379559cc59e7b191
GoreStarry <sunrise91.t3@gmail.com>
2017-07-31 13:56:57 +0800

    add trailer

--- demo-cover/index.js
+++ demo-cover/index.js
@@ -7,34 +7,43 @@ windowDetect();
 window.addEventListener('resize', windowDetect);
 
 function windowDetect() {
-  var window_proportion = window.innerWidth / window.innerHeight;
-  var container = document.querySelector('.container');
-  if (window_proportion > bg_proportion) { // use max height
+    var window_proportion = window.innerWidth / window.innerHeight;
+    var container = document.querySelector('.container');
+    if (window_proportion > bg_proportion) { // use max height
 
-    console.log(container);
-    if (container.classList) {
+        console.log(container);
+        if (container.classList) {
 
-      container.classList.add('mode--vh');
+            container.classList.add('mode--vh');
+        } else {
+            container.className += ' ' + 'mode--vh';
+        }
     } else {
-      container.className += ' ' + 'mode--vh';
-    }
-  } else {
-    if (container.classList) {
+        if (container.classList) {
 
-      container.classList.remove("mode--vh");
-    } else {
+            container.classList.remove("mode--vh");
+        } else {
 
-      container.className = container.className.replace(new RegExp('(^|\\b)' + "mode--vh" + '(\\b|$)', 'gi'), ' ');
+            container.className = container.className.replace(new RegExp('(^|\\b)' + "mode--vh" + '(\\b|$)', 'gi'), ' ');
+        }
     }
-  }
 
 }
 
 var angle = 0;
 var hand__seconds = document.getElementById('hand__seconds');
 setInterval(function () {
-  hand__seconds.style.webkitTransform = 'rotateZ(' + angle + 'deg)';
-  hand__seconds.style.transform = 'rotateZ(' + angle + 'deg)';
-  angle += 6;
-  //  console.log(angle)
+    hand__seconds.style.webkitTransform = 'rotateZ(' + angle + 'deg)';
+    hand__seconds.style.transform = 'rotateZ(' + angle + 'deg)';
+    angle += 6;
+    //  console.log(angle)
 }, 1000)
+
+
+function setTrailer() {
+    document.getElementById('img_trailer').setAttribute('src', './images/animation.gif');
+}
+
+function removeTrailer() {
+    document.getElementById('img_trailer').setAttribute('src', '');
+}
